0cb249b3c421ee0ddbdd1752eb036ed23d84c087
yd171 <yd171@duke.edu>
2022-09-26 21:27:01 -0400

    assignment 41

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -4,13 +4,14 @@
 
 void reverse(char * str) {
   //WRITE ME!
-  int i, len, temp;
-  len = strlen(str) - 1;
+  int i, len;
+  len = strlen(str);
+  char temp;
 
   for (i = 0; i < len / 2; i++) {
-    temp = str[i];
-    str[i] = str[len - i - 1];
-    str[len - i - 1] = temp;
+    temp = *(str + i);
+    *(str + i) = *(str + len - i - 1);
+    *(str + len - i - 1) = temp;
   }
 }
 
