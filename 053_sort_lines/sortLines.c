f02cdbd312055fc21ecd80d205be4d76137c3dc7
yd171 <yd171@duke.edu>
2022-10-02 17:45:59 -0400

    commit assignment 053

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,42 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void std_input(FILE * f) {
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t sz;
+  size_t i = 0;
+  while (getline(&curr, &sz, f) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < 1; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
-
+  if (argc == 1) {
+    std_input(stdin);
+  }
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], " r");
+      if (f == NULL)
+        return EXIT_FAILURE;
+      std_input(f);
+      if (fclose(f) != 0) {
+        fprintf(stderr, "cannot close file %s", argv[i]);
+        return EXIT_FAILURE;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
