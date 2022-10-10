6892cbd43eb094cf8850e26ca5b444bfdb528835
yd171 <yd171@duke.edu>
2022-10-09 23:18:12 -0400

    commit assignment 056

--- 056_kvs/kv.c
+++ 056_kvs/kv.c
@@ -20,35 +20,36 @@ kvarray_t * readKVs(const char * fname) {
 
   char * linep = NULL;
   size_t linesz = 0, i = 0;
-  char * curr = NULL;
-  while (getline(&curr, &linesz, f) >= 0) {
+  while (getline(&linep, &linesz, f) >= 0) {
     kvarray->size++;
     kvarray->kv_pairs =
-        realloc(kvarray->kv_pairs, kvarray->size * sizeof(kvarray->kv_pairs));
-
-    linep = curr;
-    size_t len_k, len_t;
+        realloc(kvarray->kv_pairs, kvarray->size * sizeof(*kvarray->kv_pairs));
+    //size_t len_k, len_t;
     char *kq, *vq;
     kq = strchr(linep, '=');
     vq = strchr(linep, '\n');
-    len_k = kq - linep;
-    len_t = vq - kq - 1;
+    //len_k = kq - linep;
+    //len_t = vq - kq - 1;
     kvpair_t * kv = malloc(sizeof(*kv));
-    kv->key = malloc((len_k + 1) * sizeof(kv->key));
-    kv->value = malloc((len_t + 1) * sizeof(kv->value));
-    strncpy(kv->key, linep, len_k);
-    kv->key[len_k] = '\0';
-    strncpy(kv->value, kq + 1, len_t);
-    kv->value[len_t] = '\0';
+    //kv->key = malloc((len_k + 1) * sizeof(kv->key));
+    //kv->value = malloc((len_t + 1) * sizeof(kv->value));
+    //strncpy(kv->key, linep, len_k);
+    kv->key = linep;
+    kv->value = kq + 1;
+    *kq = '\0';
+    *vq = '\0';
+    //kv->key[len_k] = '\0';
+    //strncpy(kv->value, kq + 1, len_t);
+    //kv->value[len_t] = '\0';
     kvarray->kv_pairs[i] = kv;
     i++;
-    curr = NULL;
+    linep = NULL;
   }
   if (fclose(f) != 0) {
     fprintf(stderr, "Could not close file");
     exit(EXIT_FAILURE);
   }
-  free(curr);
+  free(linep);
   return kvarray;
 }
 
@@ -56,7 +57,7 @@ void freeKVs(kvarray_t * pairs) {
   //WRITE ME
   for (size_t i = 0; i < pairs->size; i++) {
     free(pairs->kv_pairs[i]->key);
-    free(pairs->kv_pairs[i]->value);
+    //free(pairs->kv_pairs[i]->value);
     free(pairs->kv_pairs[i]);
   }
   free(pairs->kv_pairs);
