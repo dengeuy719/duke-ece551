77e54cd4872e6da9a0f6e29b7ad7e01ac5ea7332
yd171 <yd171@duke.edu>
2022-10-10 23:51:28 -0400

    commit assignment 059

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -1,38 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "kv.h"
+
 #include "counts.h"
+#include "kv.h"
 #include "outname.h"
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
   //WRITE ME
-  return NULL;
+  FILE * f = fopen(filename, "r");
+  if (f == NULL) {
+    fprintf(stderr, "could not open the file");
+    exit(EXIT_FAILURE);
+  }
+
+  counts_t * ans = createCounts();
+  char * line = NULL;
+  size_t sz = 0;
+  while (getline(&line, &sz, f) >= 0) {
+    char * p = strchr(line, '\n');
+    if (p != NULL) {
+      *p = '\0';
+    }
+    addCount(ans, lookupValue(kvPairs, line));
+    line = NULL;
+  }
+  free(line);
+  if (fclose(f) != 0) {
+    fprintf(stderr, "could not close file \n  FileName: %s\n", filename);
+    exit(EXIT_FAILURE);
+  }
+  return ans;
 }
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
-
- //count from 2 to argc (call the number you count i)
-
+  if (argc < 2) {
+    fprintf(stderr, "need more arguments\n");
+    exit(EXIT_FAILURE);
+  }
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  kvarray_t * kv = readKVs(argv[1]);
+  //count from 2 to argc (call the number you count i)
+  for (int i = 2; i < argc; i++) {
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
     //   (call this result c)
-
+    counts_t * c = countFile(argv[i], kv);
     //compute the output file name from argv[i] (call this outName)
-
-
+    char * outName = computeOutputFileName(argv[i]);
     //open the file named by outName (call that f)
-
+    FILE * f = fopen(outName, "w");
     //print the counts from c into the FILE f
-
+    printCounts(c, f);
     //close f
-
+    if (fclose(f) != 0) {
+      fprintf(stderr, "could not close the file\n");
+      exit(EXIT_FAILURE);
+    }
     //free the memory for outName and c
-
-
-
- //free the memory for kv
-
+    free(outName);
+    freeCounts(c);
+  }
+  //free the memory for kv
+  free(kv);
   return EXIT_SUCCESS;
 }
