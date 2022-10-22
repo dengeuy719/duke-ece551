1bfd680d2e035346fe5f6041a359beb8b53b8201
yd171 <yd171@duke.edu>
2022-10-22 15:51:45 -0400

    commit assignment 070

---
+++ 070_circle/circle.cpp
@@ -0,0 +1,32 @@
+#include "circle.hpp"
+
+#include <cmath>
+#include <cstdlib>
+double Circle::intersectionArea(const Circle & otherCircle) {
+  double distance = center.distanceFrom(otherCircle.center);
+  double r1 = radius, r2 = otherCircle.radius;
+  double s, l;
+  if (r1 > r2) {
+    l = r1;
+    s = r2;
+  }
+  else {
+    l = r2;
+    s = r1;
+  }
+  if (distance >= r1 + r2) {
+    return 0.0;
+  }
+  else if (distance < l - s) {
+    return M_PI * s * s;
+  }
+  else {
+    double a1 = std::acos(s * s - l * l + distance * distance / (2 * distance * s));
+    double a2 = std::acos(l * l - s * s + distance * distance / (2 * distance * l));
+    double ans = a1 * s * s + a2 * l * l - s * distance * std::sin(a1);
+    return ans;
+  }
+}
+void Circle::move(double dx, double dy) {
+  center.move(dx, dy);
+}
