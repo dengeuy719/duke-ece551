ded56bd3bf0b9423574d4392222a1dc2b7094205
yd171 <yd171@duke.edu>
2022-10-25 20:40:27 -0400

    finish assignment 079

---
+++ 079_sort_cpp/sortLines.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+void sortLines(std::istream & is) {
+  std::string line;
+  std::vector<std::string> res;
+  while (std::getline(is, line))
+    res.push_back(line);
+  std::sort(res.begin(), res.end());
+  for (size_t i = 0; i < res.size(); i++) {
+    std::cout << res[i];
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc == 1) {
+    sortLines(std::cin);
+  }
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      std::ifstream ifs(argv[i]);
+      if (ifs.is_open()) {
+        sortLines(ifs);
+        ifs.close();
+      }
+      else {
+        std::cerr << "cannot open file";
+      }
+    }
+  }
+  return EXIT_SUCCESS;
+}
