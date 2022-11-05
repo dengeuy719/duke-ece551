8db841c93cf96929f12fd6253cf79fe5a3433ed4
yd171 <yd171@duke.edu>
2022-11-05 17:23:12 -0400

    finish assignment 098

---
+++ 098_bst_map/bstmap.h
@@ -0,0 +1,114 @@
+#ifndef _BSTMAP_H__
+#define _BSTMAP_H__
+
+#include <cstdlib>
+#include <stdexcept>
+
+#include "assert.h"
+#include "map.h"
+
+template<typename K, typename V>
+class BstMap : public Map<K, V> {
+ private:
+  class Node {
+   public:
+    K key;
+    V value;
+    Node * left;
+    Node * right;
+    Node(K key, V value) : key(key), value(value), left(NULL), right(NULL) {}
+  };
+  Node * root;
+
+  static Node * const & find(Node * const & cur, const K & key) {
+    if (cur == NULL) {
+      return cur;
+    }
+
+    if (key < cur->key) {
+      return find(cur->left, key);
+    }
+    else if (key > cur->key) {
+      return find(cur->right, key);
+    }
+    else {
+      return cur;
+    }
+  }
+  static Node *& find(Node *& cur, const K & key) {
+    return const_cast<Node *&>(find(const_cast<Node * const &>(cur), key));
+  }
+  const K minGreaterKey(Node *& cur) {
+    assert(cur != NULL);
+    assert(cur->right != NULL);
+
+    Node * finder = cur->right;
+    K res = finder->key;
+
+    while (finder->left != NULL) {
+      finder = finder->left;
+      res = finder->key;
+    }
+    return res;
+  }
+  void deleteall(Node *& cur) {
+    if (cur != NULL) {
+      deleteall(cur->left);
+      deleteall(cur->right);
+      delete cur;
+    }
+  }
+
+ public:
+  BstMap() : root(NULL) {}
+
+  void add(const K & key, const V & value) {
+    Node *& nodeToAdd = find(root, key);
+    if (nodeToAdd == NULL) {
+      nodeToAdd = new Node(key, value);
+    }
+    else {
+      nodeToAdd->value = value;
+    }
+  }
+
+  const V & lookup(const K & key) const throw(std::invalid_argument) {
+    Node * const & nodeToLookUp = find(root, key);
+    if (nodeToLookUp == NULL) {
+      throw std::invalid_argument("cannot find key");
+    }
+    else {
+      return nodeToLookUp->value;
+    }
+  }
+
+  void remove(const K & key) {
+    Node *& nodeToRemove = find(root, key);
+    if (nodeToRemove == NULL) {
+      throw std::invalid_argument("cannot find key");
+    }
+    else {
+      if (nodeToRemove->left == NULL) {
+        Node * temp = nodeToRemove->right;
+        delete nodeToRemove;
+        nodeToRemove = temp;
+      }
+      else if (nodeToRemove->right == NULL) {
+        Node * temp = nodeToRemove->left;
+        delete nodeToRemove;
+        nodeToRemove = temp;
+      }
+      else {
+        K mgKey = minGreaterKey(nodeToRemove);
+        V mgValue = lookup(mgKey);
+        remove(mgKey);
+        nodeToRemove->key = mgKey;
+        nodeToRemove->value = mgValue;
+      }
+    }
+  }
+
+  ~BstMap<K, V>() { deleteall(root); }
+};
+
+#endif
---
+++ 098_bst_map/test.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#include "bstmap.h"
+int main(void) {
+  BstMap<int, int> map;
+  map.add(60, 1);
+  map.add(19, 2);
+  map.add(93, 3);
+  map.add(4, 4);
+  map.add(25, 5);
+  map.add(84, 6);
+  map.add(1, 7);
+  map.add(11, 8);
+  map.add(21, 9);
+  map.add(35, 10);
+  map.add(70, 11);
+  map.add(86, 12);
+  int find1 = map.lookup(60);
+  std::cout << "find1 should be 1 and is " << find1 << std::endl;
+  int find2 = map.lookup(86);
+  std::cout << "find1 should be 12 and is " << find2 << std::endl;
+  map.remove(19);
+  return EXIT_SUCCESS;
+}
