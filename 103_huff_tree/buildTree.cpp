1580429d85af7139f1d0bb0489b1aa98f8766c2b
yd171 <yd171@duke.edu>
2022-11-13 20:52:42 -0500

    finish assignment 103

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t q;
+  for (size_t i = 0; i < 257; ++i) {
+    if (counts[i] != 0) {
+      q.push(new Node(i, counts[i]));
+    }
+  }
+  while (q.size() > 1) {
+    Node * l = q.top();
+    q.pop();
+    Node * r = q.top();
+    q.pop();
+    Node * newNode = new Node(l, r);
+    q.push(newNode);
+  }
+  Node * root = q.top();
+  return root;
+}
