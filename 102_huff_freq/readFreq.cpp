001758c0fb2aae2632d8126746199f9c6ec48ccd
yd171 <yd171@duke.edu>
2022-11-13 20:00:41 -0500

    finish assignment 102

--- 102_huff_freq/readFreq.cpp
+++ 102_huff_freq/readFreq.cpp
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +23,13 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  uint64_t * ans = new uint64_t[257]();
+  std::ifstream is(fname);
+  char c;
+  while (is.get(c)) {
+    unsigned uc = c;
+    ans[uc]++;
+  }
+  ans[256] = 1;
+  return ans;
 }
