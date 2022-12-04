2a628002983d28b4c8decbc1380da01270ee06a4
yd171 <yd171@duke.edu>
2022-12-04 16:20:24 -0500

    093

--- 093_eval3/cyoa-step1.cpp
+++ 093_eval3/cyoa-step1.cpp
@@ -8,10 +8,6 @@ int main(int argc, char ** argv) {
     std::cerr << "Need 2 arguments to run" << std::endl;
     exit(EXIT_FAILURE);
   }
-  // ifstream f(argv[1], ifstream::in);
-  // if (!f) {
-  //   error("Cannot open the file");
-  // }
   Story s(argv[1]);
   s.display();
   return EXIT_SUCCESS;
