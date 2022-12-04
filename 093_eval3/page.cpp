2c3f9ba4af2b50df95f01cf7bd08c4e4e7f73e59
yd171 <yd171@duke.edu>
2022-12-04 01:09:19 -0500

    093

--- 093_eval3/page.cpp
+++ 093_eval3/page.cpp
@@ -25,11 +25,12 @@ std::vector<size_t> Page::printPage(
     std::cout << "What would you like to do?" << std::endl << std::endl;
     if (proMode) {
       for (size_t i = 0; i < choices.size(); i++) {
-        // if (choices[i].getCondition().first.size() == 0) {
-        // validInput.push_back(i + 1);
-        // std::cout << ' ' << i + 1 << ". " << choices[i].getSentence() << std::endl;
-        // continue;
-        //}
+        if (choices[i].getCondition().first.empty() &&
+            choices[i].getCondition().second == 0) {
+          validInput.push_back(i + 1);
+          std::cout << ' ' << i + 1 << ". " << choices[i].getSentence() << std::endl;
+          continue;
+        }
         bool condMet = false;
         bool noPair = true;
         for (size_t j = 0; j < gotItem.size(); j++) {
